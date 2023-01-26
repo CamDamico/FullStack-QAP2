3143447dce42b8da9ad54110b0836f50cde919b7
CamDamico <camerondamico2001@gmail.com>
2023-01-26 14:33:21 -0330

    index, routes, and logevents code written

---
+++ index.js
@@ -0,0 +1,47 @@
+// HTTP server
+
+const http = require("http");
+
+// routes.js is it's own file
+const routes = require("./routes.js");
+
+const server = http.createServer((request, response) => {
+  let path = "./views/";
+
+  switch (request.url) {
+    case "/":
+      path += "index.html";
+      response.statusCode = 200;
+      routes.indexPage(path, request.url, response);
+      break;
+    case "/about":
+      path += "about.html";
+      response.statusCode = 200;
+      routes.aboutPage(path, request.url, response);
+      break;
+    case "/contact":
+      path += "contact.html";
+      response.statusCode = 200;
+      routes.contactPage(path, request.url, response);
+      break;
+    case "/subscribe":
+      path += "subscribe.html";
+      response.statusCode = 200;
+      routes.subscribePage(path, request.url, response);
+      break;
+    case "/products":
+      path += "subscribe.html";
+      response.statusCode = 200;
+      routes.productsPage(path, request.url, response);
+      break;
+    default:
+      path += "404.html";
+      response.statusCode = 404;
+      routes.fourOffourPage(path, request.url, response);
+      break;
+  }
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening on port 3000.");
+});
---
+++ routes.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+//load the logEvents module
+const logEvents = require("./logEvents");
+
+myEmitter.addListener("route", (event, level, msg) => {
+  const d = new Date();
+  console.log(d.toLocaleString() + " * " + level.toUpperCase() + " * " + msg);
+  logEvents(event, level.toUpperCase(), msg);
+});
+
+// Index page
+
+function indexPage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit("route", event, "information", "the home page was visited.");
+}
+
+// About page
+
+function aboutPage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit("route", event, "information", "the about page was visited.");
+}
+
+// Contact page
+
+function contactPage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit(
+    "route",
+    event,
+    "information",
+    "the contact page was visited."
+  );
+}
+
+// Subscribe page
+
+function subscribePage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit(
+    "route",
+    event,
+    "information",
+    "the subscribe page was visited."
+  );
+}
+
+// Products page
+
+function productsPage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit(
+    "route",
+    event,
+    "information",
+    "the products page was visited."
+  );
+}
+
+// 404 page
+
+function fourOffourPage(path, event, response) {
+  displayFile(path, response);
+  myEmitter.emit(
+    "route",
+    event,
+    "error",
+    "a routing error occured for the " + event + "route."
+  );
+}
+
+function displayFile(path, response) {
+  fs.readFile(path, function (err, data) {
+    if (err) {
+      console.log(err);
+      response.end();
+    } else {
+      response.writeHead(response.statusCode, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    }
+  });
+}
+
+module.exports = {
+  indexPage,
+  aboutPage,
+  contactPage,
+  subscribePage,
+  fourOffourPage,
+  productsPage,
+};
